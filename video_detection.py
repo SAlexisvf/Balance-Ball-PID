999848cba51cce30501953c7eafcca9f491c3fab
Salvador Alexis Virgen Flores <salvadoralexisvirgenflores@Salvadors-MacBook-Pro.local>
2021-02-01 17:33:50 -0600

    ball not on the plate case

--- video_detection.py
+++ video_detection.py
@@ -2,11 +2,14 @@ import numpy as np
 import imutils
 import cv2
 
-cap = cv2.VideoCapture('data/test_video_1.mp4')
+cap = cv2.VideoCapture('data/test_video_2.mp4')
 
 # color range
 lower_range = np.array([117, 0, 216])
 upper_range = np.array([179, 255, 255])
+
+# ball countour area
+max_area = 6000
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -24,22 +27,25 @@ while(cap.isOpened()):
         c = max(contours, key = cv2.contourArea)
         x,y,w,h = cv2.boundingRect(c)
 
-        # draw the biggest contour (c) in green
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
-        M = cv2.moments(c)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+        if cv2.contourArea(c) < max_area:
+            print('The ball is not on the plate \n')
         
-        cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
-        cv2.putText(frame, "center", (cX-20,cY-20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+        else:
+            # draw the biggest contour (c) in green
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
-        print('cX: ', cX)
-        print('cY: ', cY)
-        print()
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            
+            cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+            cv2.putText(frame, "center", (cX-20,cY-20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+
+            print('cX: ', cX)
+            print('cY: ', cY, '\n')
 
     cv2.imshow('Original video', frame)
-    cv2.imshow('Mask detection', mask)
+    # cv2.imshow('Mask detection', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
