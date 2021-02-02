6c6fac7995c9ef7dbc7df0c4f321a74d585cf8c2
Aurora Tijerina <auro.tj@gmail.com>
2021-02-02 12:27:55 -0600

    Reorganized folders and changed the claibration program

--- balanceBallControl/hsv_picker.py
+++ balanceBallControl/hsv_picker.py
@@ -7,49 +7,60 @@ def nothing(x):
     pass
 
 # Initializing the webcam feed.
-cap = cv2.VideoCapture('data/test_video_1.mp4')
+cap = cv2.VideoCapture(0)
+# cap = cv2.imread("data/test_image_1.jpeg")
 cap.set(3,1280)
 cap.set(4,720)
 
+def mouseHSV(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN: #checks mouse left button down condition
+        global colors
+        colors = hsv[y,x]
+        print("HSV Format: ", colors)
+        print("Coordinates of pixel: X: ",x,"Y: ",y)
+
 # Create a window named trackbars.
 cv2.namedWindow("Trackbars")
+cv2.namedWindow("Original")
+
+# Create mouse callback
+cv2.setMouseCallback('Original', mouseHSV)
 
 # Now create 6 trackbars that will control the lower and upper range of 
 # H,S and V channels. The Arguments are like this: Name of trackbar, 
 # window name, range,callback function. For Hue the range is 0-179 and
 # for S,V its 0-255.
-cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("H", "Trackbars", 0, 100, nothing)
+cv2.createTrackbar("S", "Trackbars", 0, 100, nothing)
+cv2.createTrackbar("V", "Trackbars", 0, 100, nothing)
+
+colors = [0, 0, 0]
  
 while True:
-    
     # Start reading the webcam feed frame by frame.
     ret, frame = cap.read()
     if not ret:
         break
+
     # Flip the frame horizontally (Not required)
     frame = cv2.flip( frame, 1 ) 
     
     # Convert the BGR image to HSV image.
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    # Get the new values of the trackbar in real time as the user changes 
+    # Get the new epsilon values of the trackbar in real time as the user changes 
     # them
-    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
-    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
-    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
-    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
-    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
-    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+    h_epsilon = cv2.getTrackbarPos("H", "Trackbars")
+    s_epsilon = cv2.getTrackbarPos("S", "Trackbars")
+    v_epsilon = cv2.getTrackbarPos("V", "Trackbars")
  
     # Set the lower and upper HSV range according to the value selected
     # by the trackbar
-    lower_range = np.array([l_h, l_s, l_v])
-    upper_range = np.array([u_h, u_s, u_v])
+    lower_range = np.array([max(0, colors[0]*(1-h_epsilon/100)), max(0, colors[1]*(1-s_epsilon/100)), max(0, colors[2]*(1-v_epsilon/100))]).astype(int)
+    upper_range = np.array([min(colors[0]*(1+h_epsilon/100), 179), min(colors[1]*(1+s_epsilon/100), 255), min(colors[2]*(1+v_epsilon/100), 255)]).astype(int)
+
+    print(lower_range)
+    print(upper_range)
     
     # Filter the image and get the binary mask, where white represents 
     # your target color
@@ -63,10 +74,11 @@ while True:
     mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
     
     # stack the mask, orginal frame and the filtered result
-    stacked = np.hstack((mask_3,frame,res))
+    stacked = np.hstack((mask_3,res))
     
     # Show this stacked frame at 40% of the size.
-    cv2.imshow('Trackbars',cv2.resize(stacked,None,fx=0.4,fy=0.4))
+    cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.4, fy=0.4))
+    cv2.imshow('Original', frame)
     
     # If the user presses ESC then exit the program
     key = cv2.waitKey(1)
@@ -75,13 +87,10 @@ while True:
     
     # If the user presses `s` then print this array.
     if key == ord('s'):
-        
-        thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
+        thearray = [colors,[h_epsilon, s_epsilon, v_epsilon]]
         print(thearray)
-        
         # Also save this array as penval.npy
-        np.save('hsv_value',thearray)
-        break
+        np.save('hsv_value', thearray)
     
 # Release the camera & destroy the windows.    
 cap.release()
